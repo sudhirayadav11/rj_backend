f86bce72958bc60ac5a5bfa80dad05db0bac3e3c
sudhiraYadav <sudhirayadav4@gmail.com>
2024-04-21 21:01:25 +0545

    removed stripe payment

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const cors = require("cors");
 const { default: axios } = require("axios");
 
 const PORT = process.env.PORT || 5000;
-const stripe = require("stripe")(
-  "sk_test_51P0M0uA9t10f3qsTRjRlUvSsoI30qIXNY8EOTmULUvRkVsviTjGQTepfKjhXsSwiwxtA39tfowqTRop7lWMCHDW300utiaxmBC"
-);
+
 
 // databases connections
 const ConnectDB = require("./DataBase/ConnectDb");
@@ -70,36 +68,7 @@ app.use("/api/v1", orderRouter);
 
 
 
-//  stripe Payments api routes
-app.post("/api/v1/checkout", async (req, res) => {
-  try {
-    const productsData = JSON.parse(req.body.body);
-    const lineItems = productsData.map((product) => ({
-      price_data: {
-        currency: "inr",
-        product_data: {
-          name: product.name,
-        },
-        unit_amount: product.price * 100,
-      },
-      quantity: product.qty,
-    }));
-    console.log("lineItems data", lineItems);
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      line_items: lineItems,
-      mode: "payment",
-      success_url: "http://localhost:5173/success", // Change the success and cancel URLs as needed
-      cancel_url: "http://localhost:5173/cancel",
-    });
-
-    res.json({ id: session.id });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
 
 
 
